e71b65cf47fb655ef3c98276991bb2221215fe4a
엄윤호 <kolgu96@naver.com>
2024-09-05 01:08:44 +0900

    style(client/qna): css수정
    비밀번호가 걸려있을 때 나오는 key.png가 안 나오는 문제 수정
    Fotter/가 바닥에 안 나오는 문제를 해결하기 위해 tailwind css로 변경 후 flex-1 적용

--- varletclient/src/component/qna/Qna.js
+++ varletclient/src/component/qna/Qna.js
@@ -94,119 +94,104 @@ function Qna() {
   return (
     <>
       <Heading />
-      <div className="QnaMain" style={{ marginTop: "80px" }}>
-        <div className="QnaPage">
-          <div className="qnalist">
-            <div style={{ display: "flex", justifyContent: "space-between" }}>
-              <div className="qnacenter">고객센터</div>
-              <button
-                className="button1"
+      <div className="relative z-10 bg-white bg-opacity-70 p-5 rounded-lg w-[1100px] mx-auto shadow-lg text-center pt-[100px] mt-[80px] flex-1">
+        <div className="w-[1000px] mx-auto flex flex-col items-center">
+          <div className="flex justify-between w-full mb-5">
+            <div className="text-5xl font-black -mt-[90px]">고객센터</div>
+            <button
+              className="bg-blue-500 text-white h-10 rounded-md text-lg px-5 transition duration-300 ease-in-out hover:bg-blue-600 mr-7 -mt-[70px]"
+              onClick={() => {
+                writeQna();
+              }}
+            >
+              1:1 문의 작성
+            </button>
+          </div>
+          <div className="w-full border-b border-black flex justify-between font-black">
+            <div className="flex-[2]">번호</div>
+            <div className="flex-[4]">제목</div>
+            <div className="flex-[3]">등록일</div>
+            <div className="flex-[1.5]">답변여부</div>
+          </div>
+          {qnaList
+            ? qnaList.map((qna, idx) => {
+                return (
+                  <div
+                    className="w-full py-2 border-b border-gray-300 flex justify-between text-center"
+                    key={idx}
+                  >
+                    <div
+                      className="flex-[2] cursor-pointer"
+                      style={{ flex: "2", cursor: "pointer" }}
+                    >
+                      {qna.qseq}
+                    </div>
+                    <div
+                      className="flex-[4] cursor-pointer flex items-center justify-center"
+                      onClick={() => {
+                        onQnaView(qna.qseq);
+                      }}
+                    >
+                      <span>{qna.subject}</span>
+                      {qna.security == "N" && (
+                        <img src="/api/uploads/key.png" className="w-5 h-5 ml-2" />
+                      )}
+                    </div>
+                    <div className="flex-[3]">{qna.indate.substring(0, 10)}</div>
+                    <div className="flex-[1.5]">
+                      {qna.reply ? <div>답변완료</div> : <div>질문 확인 중</div>}
+                    </div>
+                  </div>
+                );
+              })
+            : null}
+          <div className="text-center py-2 flex-1">
+            {paging.prev ? (
+              <span
+                className="cursor-pointer"
                 onClick={() => {
-                  writeQna();
+                  onPageMove(paging.beginPage - 1);
                 }}
               >
-                1:1 문의 작성
-              </button>
-            </div>
-            <div className="qnatable">
-              <div className="row1">
-                <div className="col" style={{ flex: "2" }}>
-                  번호
-                </div>
-                <div className="col" style={{ flex: "4" }}>
-                  제목
-                </div>
-                <div className="col" style={{ flex: "3" }}>
-                  등록일
-                </div>
-                <div className="col" style={{ flex: "1.5" }}>
-                  답변여부
-                </div>
-              </div>
-            </div>
-            {qnaList
-              ? qnaList.map((qna, idx) => {
-                  return (
-                    <div className="row2" key={idx}>
-                      <div
-                        className="coll"
-                        style={{ flex: "2", cursor: "pointer" }}
-                      >
-                        {qna.qseq}
-                      </div>
-                      <div
-                        className="coll"
-                        style={{ flex: "3.7", cursor: "pointer" }}
-                        onClick={() => {
-                          onQnaView(qna.qseq);
-                        }}
-                      >
-                        {qna.subject}
-                        {qna.security == "N" ? (
-                          <img src="http://localhost:8070/uploads/key.png" />
-                        ) : null}
-                      </div>
-                      <div className="coll" style={{ flex: "2" }}>
-                        {qna.indate.substring(0, 10)}
-                      </div>
-                      <div className="coll" style={{ flex: "2" }}>
-                        {qna.reply ? (
-                          <div>답변완료</div>
-                        ) : (
-                          <div>질문 확인 중</div>
-                        )}
-                      </div>
-                    </div>
-                  );
-                })
-              : null}
-            <div id="paging" style={{ textAlign: "center", padding: "10px" }}>
-              {paging.prev ? (
+                ◀
+              </span>
+            ) : (
+              <div></div>
+            )}
+            {beginend ? (
+              beginend.map((page, idx) => (
                 <span
-                  style={{ cursor: "pointer" }}
+                  className="cursor-pointer mx-1"
+                  key={idx}
                   onClick={() => {
-                    onPageMove(paging.beginPage - 1);
+                    onPageMove(page);
                   }}
                 >
-                  ◀
+                  {page}
                 </span>
-              ) : (
-                <div></div>
-              )}
-              {beginend ? (
-                beginend.map((page, idx) => (
-                  <span
-                    style={{ cursor: "pointer", margin: "0 5px" }}
-                    key={idx}
-                    onClick={() => {
-                      onPageMove(page);
-                    }}
-                  >
-                    {page}
-                  </span>
-                ))
-              ) : (
-                <span>1</span>
-              )}
-              {paging.next ? (
-                <span
-                  style={{ cursor: "pointer" }}
-                  onClick={() => {
-                    onPageMove(paging.endPage + 1);
-                  }}
-                >
-                  ▶
-                </span>
-              ) : (
-                <div></div>
-              )}
-            </div>
+              ))
+            ) : (
+              <span>1</span>
+            )}
+            {paging.next ? (
+              <span
+                className="cursor-pointer"
+                onClick={() => {
+                  onPageMove(paging.endPage + 1);
+                }}
+              >
+                ▶
+              </span>
+            ) : (
+              <div></div>
+            )}
           </div>
         </div>
       </div>
       <Footer />
     </>
   );
+  
 }
 
 export default Qna;
